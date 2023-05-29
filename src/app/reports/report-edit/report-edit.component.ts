e3f3875e1f9cc644b78251f614ec1b486bb54902
Dimnir <dimitrin@my.hit.ac.il>
2023-05-29 08:55:30 +0300

    updated looks

--- src/app/reports/report-edit/report-edit.component.ts
+++ src/app/reports/report-edit/report-edit.component.ts
@@ -51,45 +51,45 @@ export class ReportEditComponent implements OnInit, OnDestroy {
     if (!this.editMode) {
       this.reportService.addReport(report);
     }
-    this.router.navigate(['../'], { relativeTo: this.route }) // back to reports page after submit
+    this.router.navigate(['../thanks'], { relativeTo: this.route }) // back to reports page after submit
   }
 
-onCancel() { // user presses cancel - goes back to reports page
-  this.router.navigate(['../'], { relativeTo: this.route })
-}
-
-onDelete() {
-  if (this.editMode) {
-    this.reportService.deleteReport(this.id);
-    this.router.navigate(['../../'], { relativeTo: this.route }) // goes back to reports page
+  onCancel() { // user presses cancel - goes back to reports page
+    this.router.navigate(['../'], { relativeTo: this.route })
   }
-}
+
+  onDelete() {
+    if (this.editMode) {
+      this.reportService.deleteReport(this.id);
+      this.router.navigate(['../../'], { relativeTo: this.route }) // goes back to reports page
+    }
+  }
 
   private initForm() {
-  let reportDesc = ''; // init values in case of new mode
-  let reportType = null;
-  let reportPlace = null;
+    let reportDesc = ''; // init values in case of new mode
+    let reportType = null;
+    let reportPlace = null;
 
-  if (this.editMode) { // get old values if in edit mode
-    const report = this.reportService.getReport(this.id);
-    reportDesc = report.description;
-    reportType = this.reportService.parseType(report.type);
-    reportPlace = this.reportService.parseStreet(report.place);
+    if (this.editMode) { // get old values if in edit mode
+      const report = this.reportService.getReport(this.id);
+      reportDesc = report.description;
+      reportType = this.reportService.parseType(report.type);
+      reportPlace = this.reportService.parseStreet(report.place);
+    }
+    this.newReportForm = new FormGroup({ // set values to new form
+      'type': new FormControl(reportType, Validators.required),
+      'description': new FormControl(reportDesc, Validators.required),
+      'place': new FormControl(reportPlace, Validators.required)
+    });
   }
-  this.newReportForm = new FormGroup({ // set values to new form
-    'type': new FormControl(reportType, Validators.required),
-    'description': new FormControl(reportDesc, Validators.required),
-    'place': new FormControl(reportPlace, Validators.required)
-  });
-}
 
-update(e) { // selector helper function
-  this.selected = e.target.value
-}
+  update(e) { // selector helper function
+    this.selected = e.target.value
+  }
 
-// *Lists* of streets & types. eg: ['street1','street2', etc]
-types = Array.from(this.reportService.repTypeMap.values());
-places = Array.from(this.reportService.streetMap.values());
+  // *Lists* of streets & types. eg: ['street1','street2', etc]
+  types = Array.from(this.reportService.repTypeMap.values());
+  places = Array.from(this.reportService.streetMap.values());
 
 
 }
